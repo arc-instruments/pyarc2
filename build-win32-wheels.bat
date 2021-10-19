ada92d1ff91a5f05107a83e85beed72e02c83b72
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2021-10-19 01:26:43 +0100

    Ensure delvewheel is running from venv when building win32 wheels

--- build-win32-wheels.bat
+++ build-win32-wheels.bat
@@ -1,3 +1,9 @@
-python -m poetry run maturin build
-for /R %%a in (target\wheels\*.whl) DO python -m delvewheel repair %%a -w target\wheels\
+@echo off
 
+python -m poetry run maturin build
+
+IF %ERRORLEVEL% NEQ 0 (
+  EXIT /B %ERRORLEVEL%
+)
+
+for /R %%a in (target\wheels\*.whl) DO python -m poetry run delvewheel repair %%a -w target\wheels\
