f79d475c3eb78e13b6ee9130035337d94f567cb5
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2021-10-20 11:24:57 +0100

    Update win32 wheel script to properly search current dll path

--- build-win32-wheels.bat
+++ build-win32-wheels.bat
@@ -1,9 +1,9 @@
 @echo off
 
-python -m poetry run maturin build
+python -m poetry run maturin build --release
 
 IF %ERRORLEVEL% NEQ 0 (
   EXIT /B %ERRORLEVEL%
 )
 
-for /R %%a in (target\wheels\*.whl) DO python -m poetry run delvewheel repair %%a -w target\wheels\
+for /R %%a in (target\wheels\*.whl) DO python -m poetry run delvewheel repair --add-path %~dp0 %%a -w target\wheels\
