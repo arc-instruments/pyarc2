6353df6103b7150c29ee645435148dd982c55d45
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2022-06-12 01:10:35 +0100

    Build beastlink packages inside the linux wheel container script

--- build-linux-wheels.sh
+++ build-linux-wheels.sh
@@ -8,8 +8,19 @@ set -ex
 
 apt update
 
-apt-get -y install libusb-1.0 libusb-0.1
-apt install -y /io/cesys-udk-lite_1.5.1-1.deb /io/beastlink-free_1.0-1.deb
+apt-get -y install libusb-1.0 libusb-0.1 wget fakeroot build-essential git
+
+mkdir /cesys
+pushd /cesys
+git clone https://github.com/arc-instruments/beastlink-rs
+cd beastlink-rs/contrib/debian
+
+./make-debian-packages.sh
+
+popd
+
+apt-get -y install /cesys/beastlink-rs/contrib/debian/cesys-udk-lite_1.5.1-1.deb
+apt-get -y install /cesys/beastlink-rs/contrib/debian/beastlink-free_1.0-1.deb
 
 curl https://sh.rustup.rs -sSf | sh -s -- --default-toolchain stable -y
 
@@ -18,7 +29,7 @@ export WHLPLAT=manylinux_2_24_x86_64
 
 cd /io/pyarc2
 
-for PYBIN in /opt/python/cp{37,38,39,310}*/bin; do
+for PYBIN in /opt/python/cp{38,39,310}*/bin; do
     PYEXEC="${PYBIN}/python"
     PIPEXEC="${PYBIN}/pip"
     "${PIPEXEC}" install -U poetry
@@ -34,7 +45,7 @@ for PYBIN in /opt/python/cp{37,38,39,310}*/bin; do
             --manylinux off
 done
 
-for whl in /io/pyarc2/target/wheels/*.whl; do
+for whl in /io/pyarc2/target/wheels/*-linux_x86_64.whl; do
     # make sure we target the correct platform when repairing
     auditwheel repair --plat $WHLPLAT "$whl" -w /io/pyarc2/target/wheels || true
 done
