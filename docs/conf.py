18be603250277afd1757e8b5320eb431f90bdcd7
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2022-06-13 10:02:49 +0100

    Update versions to match libarc2

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, ArC Instruments Ltd'
 author = 'Spyros Stathopoulos'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.1.1'
 
 
 # -- General configuration ---------------------------------------------------
