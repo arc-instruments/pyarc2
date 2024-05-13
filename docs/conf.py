54e61f42bcc8f1ea22a1d5053d893d7cc5c63614
Spyros Stathopoulos <spyros@arc-instruments.co.uk>
2024-05-13 13:43:44 +0100

    Bump development version

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ copyright = '2022–2024, ArC Instruments Ltd'
 author = 'Spyros Stathopoulos'
 
 # The full version, including alpha/beta/rc tags
-release = '0.4.2-a0'
+release = '0.5.2-a0'
 
 
 # -- General configuration ---------------------------------------------------
